3c16515639caf31f4fdc61dec95aaca9eeb5addb
PalaiNandini <you@example.com>
2021-10-25 10:00:46 +0530

    assignment complited

---
+++ Movie.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Movie
+{
+    public static final String[] Movie_Hall = {"Audi A1","Audi A2","Audi A3"};
+    public String[] movieName = {"Blak widow ","Venom","Jumanji"}; 
+    public static final String[] genre= {"Action","Action","Adventure"};
+    public static final int No_Of_Seats[] = {259,68,99};
+    public int price[] = {500,350,450};
+    public Movie() {
+    	System.out.println("Welcome to CINEPOLIS !!");
+    	
+    }
+}
